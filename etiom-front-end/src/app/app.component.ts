eed6a6e2398720d0f506f9cc47e04e6ab115d280
SuikaT <bourney.julien@gmail.com>
2024-09-03 01:46:27 +0200

    base structure

--- etiom-front-end/src/app/app.component.ts
+++ etiom-front-end/src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './views/footer/footer.component';
 import { HeaderComponent } from './views/header/header.component';
 import { MenuComponent } from './views/menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet,
     HeaderComponent,
     FooterComponent,
     MenuComponent,
+    RouterModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
