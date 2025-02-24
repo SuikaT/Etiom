764b6a85c6617a31d7259d53387888ce9d43ec53
SuikaT <bourney.julien@gmail.com>
2025-02-24 01:53:22 +0100

    Theming

--- etiom-front-end/src/app/services/theme.service.ts
+++ etiom-front-end/src/app/services/theme.service.ts
@@ -6,16 +6,26 @@ import { CookieService } from "ngx-cookie-service";
 	providedIn: "root",
 })
 export class ThemeService {
-	private currentTheme = ThemeEnum.LIGHT;
-
 	constructor(private cookie: CookieService) {}
 
 	toggleTheme() {
-		const newTheme = this.currentTheme == ThemeEnum.LIGHT ? ThemeEnum.DARK : ThemeEnum.LIGHT;
-		document.body.classList.toggle(newTheme);
-		this.currentTheme = newTheme;
+		if (document.body.classList.contains(ThemeEnum.LIGHT)) {
+			this.applyTheme(ThemeEnum.DARK);
+		} else {
+			this.applyTheme(ThemeEnum.LIGHT);
+		}
+	}
 
-		this.cookie.set("theme", newTheme, 90);
+	applyTheme(theme: ThemeEnum) {
+		if (theme == ThemeEnum.DARK) {
+			document.body.classList.remove(ThemeEnum.LIGHT);
+			document.body.classList.add(ThemeEnum.DARK);
+		} else {
+			document.body.classList.remove(ThemeEnum.DARK);
+			document.body.classList.add(ThemeEnum.LIGHT);
+		}
+		// refresh cookie
+		this.cookie.set("theme", theme, 90);
 	}
 
 	initTheme() {
@@ -24,11 +34,7 @@ export class ThemeService {
 			theme = ThemeEnum.LIGHT;
 		}
 
-		document.body.classList.add(theme);
-		this.currentTheme = theme;
-
-		// refresh cookie
-		this.cookie.set("theme", theme, 90);
+		this.applyTheme(theme);
 	}
 
 	isValidTheme(theme: ThemeEnum) {
