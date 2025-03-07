cbb07352f3de6991615d7c46a06adfdffc39d32a
SuikaT <bourney.julien@gmail.com>
2025-03-07 02:21:01 +0100

    prettier config complete patient and patient list

--- etiom-front-end/src/app/model/interfaces/Patient.ts
+++ etiom-front-end/src/app/model/interfaces/Patient.ts
@@ -2,4 +2,10 @@ export interface Patient {
   firstname: string;
   lastname: string;
   age: number;
+  phone?: string;
+  birthDate?: Date;
+  adress?: string;
+  socialWelfare?: string;
+  healthInsurance?: string;
+  appointmentCount?: number;
 }
